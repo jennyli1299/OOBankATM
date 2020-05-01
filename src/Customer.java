2e3cd7ebca111e171d9f1f69aef76db13cf6cf38
jennyli1299 <jennyli1299@users.noreply.github.com>
2020-05-01 00:12:18 -0400

    Updates to Customer

--- src/Customer.java
+++ src/Customer.java
@@ -2,6 +2,8 @@ package src;
 
 import java.util.ArrayList;
 
+import javax.xml.crypto.Data;
+
 public class Customer extends User {
 
     // Where does DatabaseManager come into play?
@@ -9,6 +11,7 @@ public class Customer extends User {
     /* constructor */
     public Customer(String firstName, String lastName, int id, String username, String password) {
         super(firstName, lastName, id, username, password);
+        DatabaseManager.addCustomer(this);
     }
 
 
@@ -21,26 +24,61 @@ public class Customer extends User {
 
      public void openCheckingAccount(float amount, Currency c) {
         // Create Checking Account
+        CheckingAccount checkingAccount = new CheckingAccount(IBAN, amount, routingNumber, accountNumber, active, c, closingCharge, openingCharge, transferFee, withdrawalFee);
         // Add to Database
+        DatabaseManager.addCheckingAccount(checkingAccount, this);
      }
      public void openSavingsAccount(float amount, Currency c) {
         // Create Savings Account
+        SavingsAccount savingsAccount = new SavingsAccount(IBAN, amount, routingNumber, accountNumber, active, c, closingCharge, openingCharge, interest);
         // Add to Database
+        DatabaseManager.addSavingsAccount(savingsAccount, this);
      }
-     public void openSecuritiesAccount(float amount, Currency c) {
+     public boolean enoughToOpenSECAcc() {
         // Check if Customers are rich enough >$5000.00 in their Savings
-        // Create Securities Account and transfer >$1000.00 into this account but MUST keep at least $2500.00 in other accounts
+        ArrayList<Account> customerAccs = DatabaseManager.getAccounts(this.getfirstName(), this.getlastName(), "SAV");
+        float totalbalance = 0;
+        boolean over1000 = false;
+        for (Account a : customerAccs) {
+            if (a.getAccountType().equals("Savings")) {
+               totalbalance += a.getBalanceInLocalCurrency(); // What is Local Currency?
+               if (a.getBalanceInLocalCurrency() >= 1000) {
+                  over1000 = true;
+               }
+            }
+        }
+        if (totalbalance >= 5000.0 && over1000) {
+           return true;
+        }
+        else return false;
+     }
+     public void openSecuritiesAccount(Account account, float amount, Currency c) {
+        // Create Securities Account & transfer >$1000.00 into securities account from specified Account(s) but MUST keep at least $2500.00 in other accounts
+        SecurityAccount securitiesAccount = new SecurityAccount(IBAN, amount, routingNumber, accountNumber, active, c, closingCharge, openingCharge);
+        account.withdrawAmount(amount);
         // Add to Database
-
-        // Else fail
+         DatabaseManager.addSecurityAccount(securitiesAccount, this);
         // Should we keep max 1?
      }
      // Close Accounts?
 
+     public String closeAccount(Account account) {
+        boolean closed = account.close();
+        if (closed) {
+           return "Your account [" + account.getAccountNumber() + "] has been successfully closed.";
+        }
+        else {
+           return "You do not have enough balance in this account to cover the closing charge. Closure denied.";
+        }
+     }
+
 
      /* View Current Balances (database query) */
      public int viewAllBalances() {
-        ArrayList<Account> allAccounts = DatabaseManager.getAccounts(getfirstName(), getlastName(), "CheckingAccount");
+        ArrayList<Account> allAccounts = DatabaseManager.getAccounts(getfirstName(), getlastName(), "CH");
+        allAccounts.addAll(DatabaseManager.getAccounts(getfirstName(), getlastName(), "SAV"));
+        allAccounts.addAll(DatabaseManager.getAccounts(getfirstName(), getlastName(), "SEC"));
+
         return -1;
      }
 
@@ -55,6 +93,10 @@ public class Customer extends User {
      public void withdrawal(Account acc, float amount) {
         // Need to decide from which account and which currency
         // withdrawal fee
+        Float Famount = Float.valueOf(amount);
+        acc.withdrawAmount(Famount);
+        CheckingAccount chAcc = (CheckingAccount)(acc);
+        // CHARGE WITHDRAWAL FEE
      }
 
      /* Transfer Money from one Account to another */
