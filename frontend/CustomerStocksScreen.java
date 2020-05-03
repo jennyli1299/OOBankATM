0474d7c2c0472ac98a556c68e5faf8b758113858
Elton Cheung <eltonc@bu.edu>
2020-05-03 17:48:51 -0400

    Finished stock screen

--- frontend/CustomerStocksScreen.java
+++ frontend/CustomerStocksScreen.java
@@ -14,15 +14,22 @@ public class CustomerStocksScreen implements ActionListener {
     DefaultListModel<String> allStocksListModel;
     ArrayList<Stock> myStocks; // keep database query here
     DefaultListModel<String> myStocksListModel;
-
+    ArrayList<Account> securities; // keep database query here
+    DefaultListModel<String> securitiesListModel;
 
     /* UI components */
     JFrame frame;
     JLabel label;
     JList allStocksList;
     JButton buyButton;
+    JLabel quantityBuyLabel;
+    JTextField quantityBuy;
+    JComboBox securitiesBuyDropdown;
     JList myStocksList;
     JButton sellButton;
+    JLabel quantitySellLabel;
+    JTextField quantitySell;
+    JComboBox securitiesSellDropdown;
     JLabel warningLabel;
     JButton backButton;
 
@@ -58,11 +65,21 @@ public class CustomerStocksScreen implements ActionListener {
         }
 
         myStocks = allStocks;
-
         myStocksListModel = new DefaultListModel<>();
         for (Stock stock : myStocks) {
-            myStocksListModel.addElement(stock.toString());
+            // TODO need way to show number of stocks owned by customer
+            myStocksListModel.addElement(stock.toString() + " (50 shares)");
         }
+
+        /* mockup for securities */
+        securities = new ArrayList<Account>();
+        for (Account account : securities) {
+            securitiesListModel.addElement(account.toString());
+        }
+        securitiesListModel = new DefaultListModel<>();
+        securitiesListModel.addElement("<CH88888888>: $15000");
+        securitiesListModel.addElement("<US12345678>: $999999");
+        securitiesListModel.addElement("<GB87654321>: $1234");
     }
 
     private void createUI() {
@@ -80,20 +97,52 @@ public class CustomerStocksScreen implements ActionListener {
 
         /* add request button */
         buyButton = new JButton("Buy selected stock");
-        buyButton.setBounds(375, 100, 150, 50);
+        buyButton.setBounds(375, 100, 200, 50);
         buyButton.addActionListener(this);
         frame.add(buyButton);
+
+        /* add quantityBuy label */
+        quantityBuyLabel = new JLabel();
+        quantityBuyLabel.setBounds(375, 150, 100, 50);
+        quantityBuyLabel.setText("Quantity:");
+        frame.add(quantityBuyLabel);
+
+        /* add quantityBuy field */
+        quantityBuy = new JTextField();
+        quantityBuy.setBounds(475, 150, 100, 50);
+        frame.add(quantityBuy);
+
+        /* add securitiesBuy dropdown */
+        securitiesBuyDropdown = new JComboBox<>(securitiesListModel.toArray());
+        securitiesBuyDropdown.setBounds(375, 200, 200, 50);
+        frame.add(securitiesBuyDropdown);
 
         /* add myStocksList */
         myStocksList = new JList<>(myStocksListModel);
         myStocksList.setBounds(50, 325, 300, 200);
         frame.add(myStocksList);
 
-        /* add pay button */
+        /* add sell button */
         sellButton = new JButton("Sell selected stock");
         sellButton.setBounds(375, 325, 150, 50);
         sellButton.addActionListener(this);
         frame.add(sellButton);
+
+        /* add quantitySell label */
+        quantitySellLabel = new JLabel();
+        quantitySellLabel.setBounds(375, 375, 100, 50);
+        quantitySellLabel.setText("Quantity:");
+        frame.add(quantitySellLabel);
+
+        /* add quantitySell field */
+        quantitySell = new JTextField();
+        quantitySell.setBounds(475, 375, 100, 50);
+        frame.add(quantitySell);
+
+        /* add securitiesSell dropdown */
+        securitiesSellDropdown = new JComboBox<>(securitiesListModel.toArray());
+        securitiesSellDropdown.setBounds(375, 425, 200, 50);
+        frame.add(securitiesSellDropdown);
 
         /* add warning label */
         warningLabel = new JLabel();
@@ -119,29 +168,29 @@ public class CustomerStocksScreen implements ActionListener {
         } else if (e.getSource() == buyButton) {
             
             /* if selcted stock is invalid, display warning */
-            int index = allStocksList.getSelectedIndex();
-            if (index == -1) {
-                warningLabel.setText("Select a stock first.");
+            int stockIndex = allStocksList.getSelectedIndex();
+            int accountIndex = securitiesBuyDropdown.getSelectedIndex();
+            if (stockIndex == -1 || accountIndex == -1 || quantityBuy.getText().equals("")) {
+                warningLabel.setText("Select a stock, a quantity, and an account to buy.");
+
+            /* try to buy the stock */
             } else {
-                Stock stock = allStocks.get(index);
-                StockBuyScreen screen = new StockBuyScreen();
-                frame.dispose();
-                // screen.frame.setVisible(true);
+                // TODO: check if the user has enough money/there's enough stock/etc
+                // if it's okay, make the purchase and set success message in warningLabel
             }
-            
-        
         /* pay -> pay the amount on the selected due loan */
         } else if (e.getSource() == sellButton) {
 
             /* if selcted stock is invalid, display warning */
-            int index = myStocksList.getSelectedIndex();
-            if (index == -1) {
-                warningLabel.setText("Select a stock first.");
+            int stockIndex = myStocksList.getSelectedIndex();
+            int accountIndex = securitiesSellDropdown.getSelectedIndex();
+            if (stockIndex == -1 || accountIndex == -1 || quantitySell.getText().equals("")) {
+                warningLabel.setText("Select a stock, a quantity, and an account to sell.");
+
+            /* try to sell the stock */
             } else {
-                Stock stock = myStocks.get(index);
-                StockSellScreen screen = new StockSellScreen();
-                frame.dispose();
-                // screen.frame.setVisible(true);
+                // TODO: try to sell the quantity of stock and put money in chosen account
+                // if it's okay, make the purchase and set success message in warningLabel
             }
         } 
     }
